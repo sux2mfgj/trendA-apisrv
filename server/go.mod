193175df082e8e16d7e4e0519f464417eda01c0a
Shunsuke Mie <sux2mfgj@gmail.com>
2022-10-08 00:12:42 +0900

    server: implement dummy data handler

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,10 @@
 module github.com/sux2mfgj/trendy-a/server
 
 go 1.19
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/dghubble/go-twitter v0.0.0-20220816163853-8a0df96f1e6d // indirect
+	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,8 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"github.com/dghubble/go-twitter/twitter"
+	"log"
+	"net/http"
+)
 
-func main() {
-    fmt.Println("hello world")
+type Loader interface {
+	Load(string, string) ([]twitter.TrendsList, error)
 }
 
+func GetTrends(w http.ResponseWriter, r *http.Request) {
+	d := DummyLoader{}
+	trends, err := d.Load("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(trends)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Version struct {
+	Version string `json:"version"`
+}
+
+func VersionHandler(w http.ResponseWriter, _ *http.Request) {
+	v := Version{
+		Version: "0.0.1",
+	}
+
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/v0/version", VersionHandler)
+	http.HandleFunc("/v0/trends", GetTrends)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
